8f2adc40272d8e3f06e7f5d509d21cf00bba4499
Dreamsies <100276137+Dreams005@users.noreply.github.com>
2022-09-05 01:09:01 +0200

    Pre-Release Prototype Version - (v0.4) -You are now able to delete an item by clicking on the item in the table and pressing the delete button -Sorting by item type & creating a new item type will be added soon -Improved overall look of the program

--- Files/main.py
+++ Files/main.py
@@ -5,12 +5,11 @@ import customtkinter
 from tkinter import ttk
 import sqlite3
 
-customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+customtkinter.set_appearance_mode("Dark")
+customtkinter.set_default_color_theme("blue")
 
 
 class App(customtkinter.CTk):
-
     COUNT = 0
     WIDTH = 780
     HEIGHT = 520
@@ -45,9 +44,39 @@ class App(customtkinter.CTk):
         self.resizable(False, False)
         self.protocol("WM_DELETE_WINDOW", self.on_closing)  # call .on_closing() when app gets closed
 
-        # ============ create two frames ============
+        conn = sqlite3.connect('item_data_rtc.db')
 
-        # configure grid layout (2x1)
+        c = conn.cursor()
+
+        c.execute("""CREATE TABLE if not exists items (
+                    rowid integer PRIMARY KEY AUTOINCREMENT,
+                    item_name text,
+                    item_type text,
+                    date_added text,
+                    date_of_manufacture text)
+                    """)
+
+        conn.commit()
+        conn.close()
+
+        def query_database():
+            conn = sqlite3.connect('item_data_rtc.db')
+
+            c = conn.cursor()
+
+            c.execute("SELECT rowid, * FROM items")
+            records = c.fetchall()
+
+            for record in records:
+                print(record)
+
+            for record in records:
+                self.table.insert(parent='', index='end', iid=record[0], text='',
+                                  values=(record[0], record[1], record[2], record[3]))
+
+            conn.commit()
+            conn.close()
+
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
@@ -61,15 +90,13 @@ class App(customtkinter.CTk):
 
         # ============ frame_left ============
 
-        # configure grid layout (1x11)
-        self.frame_left.grid_rowconfigure(0, minsize=10)   # empty row with minsize as spacing
-        self.frame_left.grid_rowconfigure(5, weight=1)  # empty row as spacing
-        self.frame_left.grid_rowconfigure(8, minsize=20)    # empty row with minsize as spacing
-        self.frame_left.grid_rowconfigure(11, minsize=10)  # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(0, minsize=10)
+        self.frame_left.grid_rowconfigure(8, minsize=20)
+        self.frame_left.grid_rowconfigure(11, minsize=10)
 
         self.add_menu_display211 = customtkinter.CTkFrame(master=self.frame_right,
                                                           corner_radius=15,
-                                                          height=385,
+                                                          height=400,
                                                           width=600)
         self.add_menu_display211.grid(pady=15, padx=15, sticky="nws")
 
@@ -77,7 +104,7 @@ class App(customtkinter.CTk):
 
         self.table = ttk.Treeview(master=self.add_menu_display211,
                                   columns=columns,
-                                  height=14,
+                                  height=17,
                                   show='headings')
 
         self.table.column("#1", anchor="c", minwidth=50, width=50)
@@ -90,24 +117,13 @@ class App(customtkinter.CTk):
         self.table.heading('date_added', text='Date Added')
         self.table.heading('date_of_manufacture', text='Date of Manufacture')
 
-        # -----TEMP DATA-----
-        self.data = []
-
-        for index, record in enumerate(self.data):
-            self.table.insert(parent='', index='end', iid=index, text="", values=(record[0],
-                                                                                  record[1],
-                                                                                  record[2],
-                                                                                  record[3]))
-
-            App.COUNT += 1
-
         self.table.grid(row=0, column=0, sticky='nsew', padx=10, pady=10)
 
         self.table.bind('<Motion>', 'break')
 
         self.label_1 = customtkinter.CTkLabel(master=self.frame_left,
                                               text="Menu",
-                                              text_font=("Roboto Medium", -16))  # font name and size in px
+                                              text_font=("Roboto Medium", -16))
         self.label_1.grid(row=1, column=0, pady=20, padx=10)
 
         self.button_1 = customtkinter.CTkButton(master=self.frame_left,
@@ -117,7 +133,7 @@ class App(customtkinter.CTk):
                                                 border_color="#3484F0",  # alternative green: #33f05f
                                                 fg_color="#343638",
                                                 command=self.button_event1)
-        self.button_1.grid(row=2, column=0, pady=15, padx=20)
+        self.button_1.grid(row=2, column=0, pady=(15, 15), padx=20)
 
         self.button_2 = customtkinter.CTkButton(master=self.frame_left,
                                                 text="Delete Item",
@@ -128,6 +144,15 @@ class App(customtkinter.CTk):
                                                 command=self.button_event2)
         self.button_2.grid(row=3, column=0, pady=15, padx=20)
 
+        self.button_3 = customtkinter.CTkButton(master=self.frame_left,
+                                                text="Edit Item",
+                                                corner_radius=15,
+                                                border_width=1.5,
+                                                border_color="#3484F0",
+                                                fg_color="#343638",
+                                                command=self.button_event3)
+        self.button_3.grid(row=4, column=0, pady=15, padx=20)
+
         def optionmenu_callback1(choice):
             print("Type selected:", choice)
 
@@ -137,7 +162,7 @@ class App(customtkinter.CTk):
                                                         fg_color="#343638",
                                                         button_hover_color="#3484F0",
                                                         command=optionmenu_callback1)
-        self.optionmenu_1.grid(row=4, column=0, pady=15, padx=20)
+        self.optionmenu_1.grid(row=5, column=0, pady=(15, 140), padx=20)
         self.optionmenu_1.set("Sort by type")
 
         self.label_creator = customtkinter.CTkLabel(master=self.frame_left, text="Project created by:")
@@ -156,6 +181,15 @@ class App(customtkinter.CTk):
         self.frame_right.columnconfigure((0, 1), weight=1)
         self.frame_right.columnconfigure(2, weight=0)
 
+        def select_record(selected):
+            selected = self.table.focus()
+            values = self.table.item(selected, 'values')
+            print(values)
+
+        self.table.bind("<ButtonRelease-1>", select_record)
+
+        query_database()
+
     def button_event1(self):
         add_item_menu = customtkinter.CTkToplevel(self)
         add_item_menu.title("Add Item Menu")
@@ -167,10 +201,10 @@ class App(customtkinter.CTk):
 
         main_add_frame = customtkinter.CTkFrame(master=add_item_menu, corner_radius=10, height=240, width=400)
         main_add_frame.grid(row=0, column=0, sticky="nswe")
-        main_add_frame.grid_rowconfigure(0, minsize=10)  # empty row with minsize as spacing
-        main_add_frame.grid_rowconfigure(5, weight=1)  # empty row as spacing
-        main_add_frame.grid_rowconfigure(8, minsize=20)  # empty row with minsize as spacing
-        main_add_frame.grid_rowconfigure(11, minsize=10)  # empty row with minsize as spacing
+        main_add_frame.grid_rowconfigure(0, minsize=10)
+        main_add_frame.grid_rowconfigure(5, weight=1)
+        main_add_frame.grid_rowconfigure(8, minsize=20)
+        main_add_frame.grid_rowconfigure(11, minsize=10)
         main_add_frame.grid(row=0, column=0, sticky="nswe", padx=15, pady=15)
 
         entry_label1 = customtkinter.CTkLabel(master=main_add_frame, text="Item Name", text_font=("Roboto Medium", -16))
@@ -213,7 +247,6 @@ class App(customtkinter.CTk):
 
         def check_input():
 
-            now = datetime.datetime.now()
             item_name = entry1.get()
             item_type = type_option_menu.get()
             item_dom = entry3.get()
@@ -223,15 +256,44 @@ class App(customtkinter.CTk):
                                              "Please make sure you filled out "
                                              "all the fields before you press Add Item.")
             else:
-                self.table.insert(parent='', index='end', iid=App.COUNT, text="Parent", values=(App.COUNT+1, item_name,
-                                                                                                now.strftime(
-                                                                                                    "%d/%m/%Y"
-                                                                                                            ),
-                                                                                                item_dom))
-                with open('Files/Data/data.txt', 'w') as f:
-                    for line in self.data:
-                        f.write(f"{line}\n")
-                App.COUNT += 1
+                conn = sqlite3.connect('item_data_rtc.db')
+
+                c = conn.cursor()
+
+                c.execute("SELECT rowid, * FROM items")
+                records = c.fetchall()
+
+                c.execute("INSERT INTO items VALUES (:item_name, :date_added, :date_of_manufacture)",
+                          {
+                              'item_name': item_name,
+                              'date_added': datetime.datetime.now().strftime("%d/%m/%Y"),
+                              'date_of_manufacture': item_dom
+                          })
+
+                conn.commit()
+
+                conn.close()
+
+                self.table.delete(*self.table.get_children())
+
+                conn = sqlite3.connect('item_data_rtc.db')
+
+                c = conn.cursor()
+
+                c.execute("SELECT rowid, * FROM items")
+                records = c.fetchall()
+
+                for record in records:
+                    print(record)
+
+                for record in records:
+                    self.table.insert(parent='', index='end', iid=record[0], text='',
+                                      values=(record[0], record[1], record[2], record[3]))
+                    App.COUNT += 1
+
+                conn.commit()
+
+                conn.close()
 
                 add_item_menu.destroy()
                 tkinter.messagebox.showinfo("Item added Successfully",
@@ -249,54 +311,109 @@ class App(customtkinter.CTk):
         button1.grid(row=7, column=0, sticky="nwe", padx=65, pady=0)
 
     def button_event2(self):
-        del_item_menu = customtkinter.CTkToplevel(self)
-        del_item_menu.title("Del Item Menu")
-        del_item_menu.iconbitmap('Files/Images/minus.ico')
-        del_item_menu.geometry("360x300")
-        del_item_menu.resizable(False, False)
-        del_item_menu.grid_columnconfigure(0, weight=1)
-        del_item_menu.grid_rowconfigure(0, weight=1)
+        selected = self.table.selection()
+        if selected:
+            rowid = selected[0]
 
-        main_del_frame = customtkinter.CTkFrame(master=del_item_menu, corner_radius=10, height=240, width=400)
-        main_del_frame.grid(row=0, column=0, sticky="nswe")
-        main_del_frame.grid_rowconfigure(0, minsize=10)  # empty row with minsize as spacing
-        main_del_frame.grid(row=0, column=0, sticky="nswe", padx=15, pady=15)
+            conn = sqlite3.connect('item_data_rtc.db')
+            conn.execute(f"DELETE FROM items WHERE rowid = {rowid}")
 
-        entry_label1 = customtkinter.CTkLabel(master=main_del_frame, text="Item ID (1, 2, 3)",
-                                              text_font=("Roboto Medium", -16))
+            self.table.delete(rowid)
+            conn.commit()
+            conn.close()
+        else:
+            tkinter.messagebox.showerror("No item selected error",
+                                         "You have to select an item to delete from the database before deleting")
+
+    def button_event3(self):
+        edit_item_menu = customtkinter.CTkToplevel(self)
+        edit_item_menu.title("Add Item Menu")
+        edit_item_menu.iconbitmap('Files/Images/plus.ico')
+        edit_item_menu.geometry("360x520")
+        edit_item_menu.resizable(False, False)
+        edit_item_menu.grid_columnconfigure(0, weight=1)
+        edit_item_menu.grid_rowconfigure(0, weight=1)
+
+        def select_record(selected):
+            selected = self.table.focus()
+            values = self.table.item(selected)
+            print(values)
+
+        main_edit_frame = customtkinter.CTkFrame(master=edit_item_menu, corner_radius=10, height=240, width=400)
+        main_edit_frame.grid(row=0, column=0, sticky="nswe")
+        main_edit_frame.grid_rowconfigure(0, minsize=10)
+        main_edit_frame.grid_rowconfigure(5, weight=1)
+        main_edit_frame.grid_rowconfigure(8, minsize=20)
+        main_edit_frame.grid_rowconfigure(11, minsize=10)
+        main_edit_frame.grid(row=0, column=0, sticky="nswe", padx=15, pady=15)
+
+        entry_label1 = customtkinter.CTkLabel(master=main_edit_frame, text="Item Name", text_font=(
+                                                                                                "Roboto Medium", -16))
         entry_label1.grid(row=0, column=0, sticky="nswe", padx=65, pady=(40, 5))
 
-        entry_var = customtkinter.StringVar()
-
-        entry1 = customtkinter.CTkEntry(master=main_del_frame,
+        entry1 = customtkinter.CTkEntry(master=main_edit_frame,
                                         placeholder_text="",
                                         width=160,
                                         height=45,
                                         border_width=2,
-                                        corner_radius=10,
-                                        textvariable=entry_var)
-        entry1.grid(row=1, column=0, sticky="nswe", padx=65, pady=(5, 15))
+                                        corner_radius=10)
+        entry1.grid(row=1, column=0, sticky="nswe", padx=65, pady=0)
 
-        #  Expand on this when you get the table to work
+        entry_label2 = customtkinter.CTkLabel(master=main_edit_frame, text="Item Type", text_font=(
+                                                                                                "Roboto Medium", -16))
+        entry_label2.grid(row=2, column=0, sticky="nswe", padx=65, pady=(15, 5))
+
+        def type_add_optionmenu_callback(choice):
+            print("Type selected:", choice)
+
+        type_option_menu = customtkinter.CTkOptionMenu(master=main_edit_frame,
+                                                       values=['Computer', 'Video player', 'Phone', 'Camera'],
+                                                       command=type_add_optionmenu_callback,
+                                                       button_color="#565b5e",
+                                                       fg_color="#343638",
+                                                       button_hover_color="#3484F0",
+                                                       corner_radius=10)
+        type_option_menu.grid(row=3, column=0, sticky="ns", padx=65, pady=(0, 5))
+
+        entry_label3 = customtkinter.CTkLabel(master=main_edit_frame, text="Date of manufacture",
+                                              text_font=("Roboto Medium", -16))
+        entry_label3.grid(row=4, column=0, sticky="nswe", padx=65, pady=(15, 5))
+
+        entry3 = customtkinter.CTkEntry(master=main_edit_frame,
+                                        placeholder_text=select_record,
+                                        width=160,
+                                        height=45,
+                                        border_width=2,
+                                        corner_radius=10)
+        entry3.grid(row=5, column=0, sticky="nwe", padx=65, pady=0)
 
         def check_input():
-            try:
-                if len(entry1.get()) == 0:
-                    tkinter.messagebox.showerror("No input error",
-                                                 "Please input a valid ID before you press delete item.")
-                elif len(entry1.get()) >= 5:
-                    tkinter.messagebox.showerror("Number too big error",
-                                                 "There is no way you have this many items.")
-                else:
-                    print("ok")
-                    # Prints: Found it! ['a', 'c']
 
-            except ValueError:
-                tkinter.messagebox.showerror("Not a number error",
-                                             "Please input a number.")
+            now = datetime.datetime.now()
+            item_name = entry1.get()
+            item_type = type_option_menu.get()
+            item_dom = entry3.get()
 
-        button1 = customtkinter.CTkButton(master=main_del_frame,
-                                          text="Delete Item",
+            if len(item_name) == 0 or len(item_dom) == 0:
+                tkinter.messagebox.showerror("No input error",
+                                             "Please make sure you filled out "
+                                             "all the fields before you press Edit Item.")
+            else:
+                conn = sqlite3.connect('item_data_rtc.db')
+                c = conn.cursor()
+                c.execute("SELECT rowid, * FROM items")
+
+                records = c.fetchall()
+
+                conn.commit()
+                conn.close()
+
+                main_edit_frame.destroy()
+                tkinter.messagebox.showinfo("Item edited Successfully",
+                                            "That item has been edited successfully.")
+
+        button1 = customtkinter.CTkButton(master=main_edit_frame,
+                                          text="Edit Item",
                                           width=200,
                                           height=50,
                                           border_width=2,
@@ -304,7 +421,7 @@ class App(customtkinter.CTk):
                                           fg_color="#343638",
                                           corner_radius=20,
                                           command=check_input)
-        button1.grid(row=3, column=0, sticky="nsw", padx=65, pady=(50, 0))
+        button1.grid(row=7, column=0, sticky="nwe", padx=65, pady=0)
 
 
 if __name__ == "__main__":
